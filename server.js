eb99830035a9ae0a1e5bf68eba24bce9124a7584
Adam Fuentes <66355715+Adam-Fuentes@users.noreply.github.com>
2020-10-29 10:46:32 +0100

    add new "routes"

--- server.js
+++ server.js
@@ -8,6 +8,14 @@ app.get('/', function (req, res) {
 app.get('/patata', function (req, res) {
   res.send('<h1>Hello patata</h1>');
 });
+
+app.get('/about', function (req, res) {
+  res.send('<h1>Hello soy Adam</h1><p>estoy estudiando desarrollo web</p>');
+});
+
+app.get('/hobbies', function (req, res) {
+  res.send('<ul><li>Cocinar</li><li>Magic The Gatherin</li><li>Salir por ahí</li></ul>');
+});
  
 app.listen(3000, function(){
   console.log("Server started on port 3000.");
